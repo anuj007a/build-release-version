6ff4174918c0fdbc6cd90aa00c785f8f42365f93
Anuj <anuj@Anujs-MacBook-Pro.local>
2024-12-02 00:47:50 +0530

    Build release version control

---
+++ src/main/java/com/anuj/build_release_version/BuildReleaseVersionApplication.java
@@ -0,0 +1,13 @@
+package com.anuj.build_release_version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildReleaseVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildReleaseVersionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuj/build_release_version/BuildReleaseVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj.build_release_version;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildReleaseVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
